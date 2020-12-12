84c99ec6a6a2c4c72432a442e569367d8188cb47
David van Capelle <david.van.capelle@gmail.com>
2020-12-12 22:26:01 +0100

    upload opdracht arrow function

---
+++ script.js
@@ -0,0 +1,30 @@
+//opdracht deel A
+//onderstaande functie
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+//is hetzelfde als:
+
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+//Deel B
+const fivePlusSeven = function () {
+    return 5 + 7
+  };
+
+  //is hetzelfde als
+
+  const fivePlusSeven = () => 5 + 7;
+
+// nu opdracht C
+const myFunction = (a, b) => a + b;
+
+//opdracht D
+const addFive = a => a = 5;
+
+// opdracht E
+const createObject = () => ({greeting: "hoi"});
+
+
+
